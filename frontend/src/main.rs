97cc7f0c2c3106c8925e627e3ef9dfe7cf88fb12
sivertjoe <sivert-joh@hotmail.com>
2023-07-24 18:08:14 +0200

    🙈 fix: gitignore

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,23 +1,36 @@
 use vanilla::*;
 
+struct Page {
+    stuff: Vec<Element>,
+}
+
+impl Component for Page {
+    fn view(self) -> Box<dyn AsRef<Node>> {
+        Box::new(div![
+            styles! {
+                "background" => "red"
+            },
+            div![
+                styles! {
+                    "background" => "blue"
+                },
+                h1!["div1"]
+            ],
+            div![
+                styles! {
+                    "background" => "green"
+                },
+                h1!["div1"]
+            ],
+            h2!["Hello, World"],
+            self.stuff
+        ])
+    }
+}
+
 fn main() {
-    start_app(div![
-        styles! {
-            "background" => "red"
-        },
-        div![
-            styles! {
-                "background" => "blue"
-            },
-            h1!["div1"]
-        ],
-        div![
-            styles! {
-                "background" => "green"
-            },
-            h1!["div1"]
-        ],
-        h2!["Hello, World"],
-        vec![h2!["test"], h2!["sofie"], h2!["test"]],
-    ])
+    let page = Page {
+        stuff: vec![h2!["test"]],
+    };
+    start_app(page)
 }
