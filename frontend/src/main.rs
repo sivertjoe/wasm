22e983e2ae9eadf5753dd0b507e8b72a0b28bd6c
sivertjoe <sivert-joh@hotmail.com>
2023-07-24 19:53:56 +0200

    🎨 update: main example page

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,12 +1,23 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 use vanilla::*;
 
 struct Page {
     stuff: Vec<Element>,
+    id: vanilla::Uuid,
 }
 
 impl Component for Page {
-    fn view(self) -> Box<dyn AsRef<Node>> {
-        Box::new(div![
+    fn id(&self) -> String {
+        self.id.to_string()
+    }
+
+    fn view(&self) -> Element {
+        console_log("I got called?!");
+        div![
+            attrs! {
+                "id" => self.id()
+            },
             styles! {
                 "background" => "red"
             },
@@ -23,14 +34,25 @@ impl Component for Page {
                 h1!["div1"]
             ],
             h2!["Hello, World"],
-            self.stuff
-        ])
+            self.stuff.clone()
+        ]
     }
 }
 
 fn main() {
     let page = Page {
-        stuff: vec![h2!["test"]],
+        id: vanilla::Uuid::new_v4(),
+        stuff: vec![h2!["test"], h1!("Sofie")],
     };
+
+    let page = Rc::new(RefCell::new(page));
+
+    let cpy = Rc::clone(&page);
+    spawn_local(async move {
+        Timeout::new(3_000).await;
+        cpy.borrow_mut().stuff.push(h2!("new!!"));
+        console_log("update!");
+        cpy.update();
+    });
     start_app(page)
 }
